3f7d6f8963580d000510606b3e313b6ca8a6b2b5
usamaikram143 <usamaikram143@gmail.com>
2021-10-13 23:21:33 -0400

    Added some extra functionality to not add empty link without name or URL

--- src/components/Form.js
+++ src/components/Form.js
@@ -23,10 +23,23 @@ class Form extends Component {
 
         // to prevent page reload on form submit
         event.preventDefault()
+        if(this.state.name==="" && this.state.URL===""){
+            alert("Please enter Name and URL")
+        }
 
-        var link={name:this.state.name,URL:this.state.URL}
-        this.props.submit(link)
-        this.setState({name:"",URL:""})
+        else if(this.state.name===""){
+            alert("Please enter Name")
+        }
+
+        else if(this.state.URL===""){
+            alert("Please enter URL")
+        }
+
+        else{
+            var link={name:this.state.name,URL:this.state.URL}
+            this.props.submit(link)
+            this.setState({name:"",URL:""})
+        }
     }
 
     render() {
